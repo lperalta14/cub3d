1aca1705fa02db4277f12f3a4c2473cabd8ac394
lperalta <lperalta@c2r6s4.42malaga.com>
2026-03-10 20:08:47 +0100

    cambios minimos en main

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # cub3d
 Recreación básica de videojuego 3D basado en Raycasting
 by nosotros
+
+1º leer si hay argumento: hay? seguimos
+						 no hay? salimos
+2º comprobar que el archivo se abre, tiene la extensión correcta:
+3º parseamos mapa
+4º jugamos
+5º limpiamos
+6º salimos
+
+3- leer el mapa. ver texturas. ver todo el parseo del mapa con 1 y  0 y demás
+
+4- matematicas y raycasting
+
+5- comprobar qué recursos existen y limpiarlos. 
+
+6- return(0);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lperalta <lperalta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 16:51:20 by anzarago          #+#    #+#             */
-/*   Updated: 2026/03/10 19:35:08 by lperalta         ###   ########.fr       */
+/*   Updated: 2026/03/10 20:07:32 by lperalta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int main(int argc, char **argv)
 {
+	int fd;
 	//t_scene data;
 	
-	if(argc != 2 || check_file(argv[1]) == FALSE)
+	if(argc != 2)
 		return(1);
+	fd = open(argv[1], O_RDONLY);
+	if(fd < 0 || check_file(argv[1]) == FALSE)
+		return(1); //cambiar return por función de error con mensaje personalizado y cerrar fd	
 	/*data = init(argv[1]);
 	if(!data)
 		return(1);
