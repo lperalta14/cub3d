012f048e19d21a23b27c70096bc79323e8dc913e
lperalta <lperalta@c1r12s2.42malaga.com>
2026-05-08 17:14:23 +0200

    no changes

--- src/parsing/extract_information.c
+++ src/parsing/extract_information.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   extract_information.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anzarago <anzarago@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: lperalta <lperalta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 19:25:49 by anzarago          #+#    #+#             */
-/*   Updated: 2026/05/08 16:37:57 by anzarago         ###   ########.fr       */
+/*   Updated: 2026/05/08 17:01:05 by lperalta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	read_line_and_parse(t_scene *data, char *line)
 {
-	int  	i;
+	int		i;
 	
 	i = 0;
 	if(!line[i])
