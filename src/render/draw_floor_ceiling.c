6f0403ddb7aaeb75063146476d69814b18b652b3
lperalta <luisperaltamunoz@gmail.com>
2026-05-11 14:53:18 +0200

    dividida función para pintar suelo y cielo para no pasarse de lineas

--- src/render/draw_floor_ceiling.c
+++ src/render/draw_floor_ceiling.c
@@ -12,36 +12,28 @@ static uint32_t	int_to_rgba(int color)
 	b = color & 0xFF;
 	return (r << 24 | g << 16 | b << 8 | 0xFF);
 }
- 
-void	draw_floor_ceiling(t_game *game)
+
+static void	draw_half(t_game *game, uint32_t color, int y_start, int y_end)
 {
-	uint32_t	ceil_color;
-	uint32_t	floor_color;
-	int			x;
-	int			y;
- 
-	ceil_color = int_to_rgba(game->scene->ceiling);
-	floor_color = int_to_rgba(game->scene->floor);
-	y = 0;
-	while (y < WIN_H / 2)
+	int	x;
+	int	y;
+
+	y = y_start;
+	while (y < y_end)
 	{
 		x = 0;
 		while (x < WIN_W)
 		{
-			mlx_put_pixel(game->img, x, y, ceil_color);
-			x++;
-		}
-		y++;
-	}
-	while (y < WIN_H)
-	{
-		x = 0;
-		while (x < WIN_W)
-		{
-			mlx_put_pixel(game->img, x, y, floor_color);
+			mlx_put_pixel(game->img, x, y, color);
 			x++;
 		}
 		y++;
 	}
 }
- 
+
+void	draw_floor_ceiling(t_game *game)
+{
+	draw_half(game, int_to_rgba(game->scene->ceiling), 0, WIN_H / 2);
+	draw_half(game, int_to_rgba(game->scene->floor), WIN_H / 2, WIN_H);
+}
+
